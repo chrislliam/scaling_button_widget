fb93fc9908cfd57c2689b0e8164a5c5775aa02ff
chrislliam <91385593+chrislliam@users.noreply.github.com>
2025-03-01 23:01:25 -0300

    Update scaling_button_widget.dart

--- lib/scaling_button_widget.dart
+++ lib/scaling_button_widget.dart
@@ -2,7 +2,6 @@ library scaling_button_widget;
 
 import 'package:flutter/material.dart';
 
-
 /// The `ScalingButton` widget creates a button that scales down slightly when
 /// pressed and returns to its original size when released.
 class ScalingButton extends StatefulWidget {
@@ -10,20 +9,22 @@ class ScalingButton extends StatefulWidget {
   final Widget child;
 
   /// The callback function to be invoked when the button is tapped.
-  final VoidCallback onTap;
+  final VoidCallback? onTap;
 
   /// Creates a scaling button widget.
   ///
   /// The [child] parameter is required and specifies the widget to be displayed as the button.
   ///
   /// The [onTap] parameter is required and specifies the callback function to be invoked when the button is tapped.
-  const ScalingButton({Key? key, required this.onTap, required this.child}) : super(key: key);
+  const ScalingButton({Key? key, required this.onTap, required this.child})
+      : super(key: key);
 
   @override
   State<ScalingButton> createState() => _ScalingButtonState();
 }
 
-class _ScalingButtonState extends State<ScalingButton> with SingleTickerProviderStateMixin {
+class _ScalingButtonState extends State<ScalingButton>
+    with SingleTickerProviderStateMixin {
   late AnimationController _animationController;
 
   @override
@@ -47,15 +48,19 @@ class _ScalingButtonState extends State<ScalingButton> with SingleTickerProvider
       onTapDown: (_) {
         _animationController.forward();
       },
-      onTapUp: (_) {
-        _animationController.reverse();
-        widget.onTap();
-      },
+      onTapUp: widget.onTap != null
+          ? (_) {
+              _animationController.reverse();
+              widget.onTap!();
+            }
+          : null,
       onTapCancel: () {
         _animationController.reverse();
       },
-      child:
-          ScaleTransition(scale: Tween<double>(begin: 1, end: 0.9).animate(_animationController), child: widget.child),
+      child: ScaleTransition(
+          scale:
+              Tween<double>(begin: 1, end: 0.9).animate(_animationController),
+          child: widget.child),
     );
   }
 }
